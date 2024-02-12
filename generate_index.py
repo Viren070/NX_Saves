7a6cdcc0ed3e7185d7a808ac57da963347e57faa
Viren070 <viren070@protonmail.com>
2024-02-12 16:07:21 +0000

    update generate_index.py & index.md

--- generate_index.py
+++ generate_index.py
@@ -8,4 +8,4 @@ with open('index.md', 'w', encoding='utf-8') as f:
     for filename in os.listdir(base_dir):
         encoded_filename = urllib.parse.quote(filename)  # URL encode the filename
         download_url = base_url + encoded_filename
-        f.write(f'[{filename}]({download_url})\n')
+        f.write(f'[{filename}]({download_url})\n\n\n')
