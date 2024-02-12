ccfd377f1b06b97dd3d4331dfb0ba2843a220f1e
Viren070 <viren070@protonmail.com>
2024-02-12 16:33:08 +0000

    update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -7,5 +7,7 @@ base_url = 'https://github.com/Viren070/NX_Saves/raw/main/nintendo/switch/savega
 with open('index.md', 'w', encoding='utf-8') as f:
     for filename in os.listdir(base_dir):
         encoded_filename = urllib.parse.quote(filename)  # URL encode the filename
+        if "~" in filename or "_" in filename:
+            filename = filename.replace("~", "\\~")
         download_url = base_url + encoded_filename
         f.write(f'[{filename}]({download_url})\n\n\n')
