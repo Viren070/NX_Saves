fdfbd31f58b50e6439c885443e094b93d54b4b58
Viren070 <viren070@protonmail.com>
2024-02-13 12:41:21 +0000

    update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -5,7 +5,10 @@ base_dir = 'nintendo/switch/savegames'
 base_url = 'https://github.com/Viren070/NX_Saves/raw/main/nintendo/switch/savegames/'
 
 with open('index.md', 'w', encoding='utf-8') as f:
-    for filename in os.listdir(base_dir):
+    # List filenames and sort them alphabetically
+    filenames = sorted(os.listdir(base_dir))
+    
+    for filename in filenames:
         encoded_filename = urllib.parse.quote(filename)  # URL encode the filename
         if "~" in filename or "_" in filename:
             filename = filename.replace("~", "\\~")
